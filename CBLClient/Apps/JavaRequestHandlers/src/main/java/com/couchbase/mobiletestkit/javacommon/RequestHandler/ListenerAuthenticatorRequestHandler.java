f94aa7af743eaf7be44b8117d3d4017c53d18c44
mgcouchbase <manasa.ghanta@couchbase.com>
2020-08-06 11:26:15 -0700

    Listener password api

--- CBLClient/Apps/JavaRequestHandlers/src/main/java/com/couchbase/mobiletestkit/javacommon/RequestHandler/ListenerAuthenticatorRequestHandler.java
+++ CBLClient/Apps/JavaRequestHandlers/src/main/java/com/couchbase/mobiletestkit/javacommon/RequestHandler/ListenerAuthenticatorRequestHandler.java
@@ -4,6 +4,7 @@ import com.couchbase.lite.DocumentChange;
 import com.couchbase.lite.DocumentChangeListener;
 import com.couchbase.lite.ListenerPasswordAuthenticator;
 import com.couchbase.mobiletestkit.javacommon.Args;
+import com.couchbase.lite.ListenerPasswordAuthenticator;
 
 import java.util.List;
 
@@ -11,8 +12,15 @@ public class ListenerAuthenticatorRequestHandler {
 
     public ListenerPasswordAuthenticator create(Args args) {
         String username = args.get("username");
-        String password = args.get("password");
-        return ListenerPasswordAuthenticator.authenticate(username, password);
+        String pass = args.get("password");
+        char[] password = pass.toCharArray();
+        PasswordAuthenticator passwordAuthenticator = new PasswordAuthenticator();
+        ListenerPasswordAuthenticator listenerPasswordAuthenticator = ListenerPasswordAuthenticator.create(
+                passwordAuthenticator);
+        listenerPasswordAuthenticator.authenticate(username, password);
+        System.out.println(listenerPasswordAuthenticator);
+        System.out.println("PASSSSSSS");
+        return listenerPasswordAuthenticator;
     }
 
 
