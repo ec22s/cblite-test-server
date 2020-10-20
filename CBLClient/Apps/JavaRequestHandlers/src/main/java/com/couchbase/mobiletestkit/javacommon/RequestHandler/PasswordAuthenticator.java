7df4969eec7127aa9928a74df9286b9ca0f15f4a
mgcouchbase <manasa.ghanta@couchbase.com>
2020-10-20 15:02:56 -0700

    test fixes

--- CBLClient/Apps/JavaRequestHandlers/src/main/java/com/couchbase/mobiletestkit/javacommon/RequestHandler/PasswordAuthenticator.java
+++ CBLClient/Apps/JavaRequestHandlers/src/main/java/com/couchbase/mobiletestkit/javacommon/RequestHandler/PasswordAuthenticator.java
@@ -1,12 +1,10 @@
 package com.couchbase.mobiletestkit.javacommon.RequestHandler;
 
-import android.support.annotation.NonNull;
 import java.util.Arrays;
-
 import com.couchbase.lite.ListenerPasswordAuthenticatorDelegate;
 
 public class PasswordAuthenticator implements ListenerPasswordAuthenticatorDelegate {
-    public boolean authenticate(@NonNull String username, @NonNull char[] password) {
+    public boolean authenticate(String username, char[] password) {
         return (username.equals("testkit")) && Arrays.equals(password, "pass".toCharArray());
     }
 }
