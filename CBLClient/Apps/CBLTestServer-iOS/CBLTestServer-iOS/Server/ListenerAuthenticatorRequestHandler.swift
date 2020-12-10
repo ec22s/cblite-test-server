69165084a614ad7c1b057c6b2ba0903d957c05b6
mgcouchbase <manasa.ghanta@couchbase.com>
2020-12-10 10:50:13 -0800

    IOS CE app changes

--- CBLClient/Apps/CBLTestServer-iOS/CBLTestServer-iOS/Server/ListenerAuthenticatorRequestHandler.swift
+++ CBLClient/Apps/CBLTestServer-iOS/CBLTestServer-iOS/Server/ListenerAuthenticatorRequestHandler.swift
@@ -30,6 +30,7 @@ public class ListenerAuthenticatorRequestHandler {
         ////////////////////////
         // Authenticator Request Handler //
         ////////////////////////
+        #if COUCHBASE_ENTERPRISE
         case "listenerAuthenticator_create":
             let li_username: String = args.get(name: "username")!
             let li_password: String = args.get(name: "password")!
@@ -39,7 +40,6 @@ public class ListenerAuthenticatorRequestHandler {
                        (password as NSString).isEqual(to: li_password)
             }
             return listenerAuth
-        #if COUCHBASE_ENTERPRISE
         case "listenerAuth_listenerCertificateAuthenticator_create":
             let rootCertData = try dataFromResource(name: "identity2/client-ca", ofType: "der")
             let rootCert = SecCertificateCreateWithData(kCFAllocatorDefault, rootCertData as CFData)!
