121717aee2f83aee4fe7ad72a7f28dba388c9cef
mgcouchbase <manasa.ghanta@couchbase.com>
2020-12-10 15:25:12 -0800

    predective queries

--- CBLClient/Apps/CBLTestServer-iOS/CBLTestServer-iOS/Server/PredictiveQueriesRequestHandler.swift
+++ CBLClient/Apps/CBLTestServer-iOS/CBLTestServer-iOS/Server/PredictiveQueriesRequestHandler.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 import CouchbaseLiteSwift
-
+#if COUCHBASE_ENTERPRISE
 
 public class PredictiveQueriesRequestHandler {
     public static let VOID: String? = nil
@@ -16,7 +16,7 @@ public class PredictiveQueriesRequestHandler {
     
     public func handleRequest(method: String, args: Args) throws -> Any? {
         switch method {
-        #if COUCHBASE_ENTERPRISE
+        
         case "predictiveQuery_registerModel":
             let modelName: String = args.get(name: "model_name")!
             let echoModel = EchoModel(name: modelName)
@@ -116,7 +116,7 @@ public class PredictiveQueriesRequestHandler {
             }
             
             return resultArray
-        #endif
+        
         default:
             throw RequestHandlerError.MethodNotFound(method)
         }
@@ -139,3 +139,4 @@ class EchoModel: PredictiveModel {
     }
     
 }
+#endif
