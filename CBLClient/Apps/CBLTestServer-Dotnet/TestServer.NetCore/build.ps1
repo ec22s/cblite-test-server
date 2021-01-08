19c0afc7bcbdbae802f848e048f75f8b03f07382
mgcouchbase <manasa.ghanta@couchbase.com>
2021-01-08 14:42:09 -0800

    updating the client files

--- CBLClient/Apps/CBLTestServer-Dotnet/TestServer.NetCore/build.ps1
+++ CBLClient/Apps/CBLTestServer-Dotnet/TestServer.NetCore/build.ps1
@@ -97,7 +97,7 @@ try {
     
     $ZipPath = Resolve-Path ".\zips"
 
-    Push-Location bin\Release\netcoreapp2.0\publish
+    Push-Location bin\Release\netcoreapp2.1\publish
     try {
         7z a -r ${ZipPath}\TestServer.NetCore.zip *
     } finally {
