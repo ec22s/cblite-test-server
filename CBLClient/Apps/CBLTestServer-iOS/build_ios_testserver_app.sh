cb1c50f7e2c5aa308df16a6b9c0e0ea79d5f5c67
Manasa Ghanta <57646756+mgcouchbase@users.noreply.github.com>
2021-01-27 13:33:29 -0800

    Update build_ios_testserver_app.sh

--- CBLClient/Apps/CBLTestServer-iOS/build_ios_testserver_app.sh
+++ CBLClient/Apps/CBLTestServer-iOS/build_ios_testserver_app.sh
@@ -26,7 +26,7 @@ if [[ -d ${FRAMEWORK_DIR} ]]; then rm -rf ${FRAMEWORK_DIR}/*; fi
 
 pushd ${FRAMEWORK_DIR}
 pwd
-IOS_ZIP=${WORKSPACE}/artifacts/couchbase-lite-swift_${EDITION}_${VERSION}-${BLD_NUM}.zip
+IOS_ZIP=${WORKSPACE}/artifacts/couchbase-lite-swift_xc_${EDITION}_${VERSION}-${BLD_NUM}.zip
 if [[ -f ${IOS_ZIP} ]]; then
     unzip ${IOS_ZIP}
     cp -r iOS/CouchbaseLiteSwift.xcframework .
