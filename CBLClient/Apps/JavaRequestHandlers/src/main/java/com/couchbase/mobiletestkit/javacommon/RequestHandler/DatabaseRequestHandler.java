099a742d108bcf77fd82002a6de6ec8d38a04cf3
Eunice Huang <eunice.huang@couchbase.com>
2021-04-05 09:45:49 -0700

    rollback getIndexes() api call since CBLite java/adnroid rolled back

--- CBLClient/Apps/JavaRequestHandlers/src/main/java/com/couchbase/mobiletestkit/javacommon/RequestHandler/DatabaseRequestHandler.java
+++ CBLClient/Apps/JavaRequestHandlers/src/main/java/com/couchbase/mobiletestkit/javacommon/RequestHandler/DatabaseRequestHandler.java
@@ -98,7 +98,7 @@ public class DatabaseRequestHandler {
 
     public List<String> getIndexes(Args args) throws CouchbaseLiteException {
         Database database = args.get("database");
-        return database.getIndexNames();
+        return database.getIndexes();
     }
 
     public Map<String, Map<String, Object>> getDocuments(Args args) {
