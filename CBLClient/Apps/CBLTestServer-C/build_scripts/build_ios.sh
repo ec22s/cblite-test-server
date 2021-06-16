799e7040397ce796e4001d6e76be04f960130367
Jim Borden <jim.borden@couchbase.com>
2021-06-16 12:18:23 +0900

    Call cmake to pull deps before building ios

--- CBLClient/Apps/CBLTestServer-C/build_scripts/build_ios.sh
+++ CBLClient/Apps/CBLTestServer-C/build_scripts/build_ios.sh
@@ -10,17 +10,21 @@ ZIPS_DIR=$SCRIPT_DIR/../zips
 BUILD_SIM_FOLDER="build_sim"
 BUILD_DEVICE_FOLDER="build_device"
 
-rm -rf $DOWNLOAD_DIR 2> /dev/null
-mkdir -p $DOWNLOAD_DIR
+# rm -rf $DOWNLOAD_DIR 2> /dev/null
+# mkdir -p $DOWNLOAD_DIR
 
-pushd $DOWNLOAD_DIR
-ZIP_FILENAME=couchbase-lite-c-ios-${VERSION}-${BLD_NUM}-enterprise.zip
-curl -O http://latestbuilds.service.couchbase.com/builds/latestbuilds/couchbase-lite-c/${VERSION}/${BLD_NUM}/${ZIP_FILENAME}
-unzip ${ZIP_FILENAME}
-rm ${ZIP_FILENAME}
-popd
+# pushd $DOWNLOAD_DIR
+# ZIP_FILENAME=couchbase-lite-c-ios-${VERSION}-${BLD_NUM}-enterprise.zip
+# curl -O http://latestbuilds.service.couchbase.com/builds/latestbuilds/couchbase-lite-c/${VERSION}/${BLD_NUM}/${ZIP_FILENAME}
+# unzip ${ZIP_FILENAME}
+# rm ${ZIP_FILENAME}
+# popd
 
 pushd $SCRIPT_DIR/../ios/TestServer
+mkdir -p cmake
+cd cmake
+cmake ../../../shared
+cd ..
 xcodebuild -scheme TestServer -sdk iphonesimulator -configuration Release -derivedDataPath $BUILD_SIM_FOLDER | xcpretty
 xcodebuild -scheme TestServer -sdk iphoneos -configuration Release -derivedDataPath $BUILD_DEVICE_FOLDER -allowProvisioningUpdates | xcpretty
 popd
