e7bf16bffb5bff743e16fc7cc0af2e9951d00f27
euniceatcouchbase <eunice.huang@couchbase.com>
2021-07-06 16:06:08 -0700

    fix java/android build due to CBLite changes

--- CBLClient/Apps/JavaRequestHandlers/src/main/java/com/couchbase/mobiletestkit/javacommon/RequestHandler/CollatorRequestHandler.java
+++ CBLClient/Apps/JavaRequestHandlers/src/main/java/com/couchbase/mobiletestkit/javacommon/RequestHandler/CollatorRequestHandler.java
@@ -8,13 +8,13 @@ public class CollatorRequestHandler {
 
     public Collation.ASCII ascii(Args args) {
         Boolean ignoreCase = args.get("ignoreCase");
-        return Collation.ascii().ignoreCase(ignoreCase);
+        return Collation.ascii().setIgnoreCase(ignoreCase);
     }
 
     public Collation.Unicode unicode(Args args) {
         Boolean ignoreCase = args.get("ignoreCase");
         Boolean ignoreAccents = args.get("ignoreAccents");
-        return Collation.unicode().ignoreCase(ignoreCase).ignoreAccents(ignoreAccents);
+        return Collation.unicode().setIgnoreCase(ignoreCase).setIgnoreAccents(ignoreAccents);
     }
 
 }
