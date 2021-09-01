0c1823b8a3ed07885f728a64fa105880bd1049ec
Jim Borden <jim.borden@couchbase.com>
2021-09-01 14:03:37 +0900

    CBL-2309: Fix library loading for squirrely Android weirdness
    On older versions of Android, the full path is needed, but on later versions the full path is basically not allowed.  To further complicate things the emulators seem to not care either way, which can throw you off the scent of the real problem.

--- CBLClient/Apps/CBLTestServer-C/src/Android/DummyLoader.cpp
+++ CBLClient/Apps/CBLTestServer-C/src/Android/DummyLoader.cpp
@@ -8,11 +8,16 @@
 #define LIB_PATH "/data/data/com.couchbase.testsuite/lib/"
 
 void* load_lib(const char* path) {
-    void* handle = dlopen(path, RTLD_NOW | RTLD_GLOBAL);
+    static char buffer[PATH_MAX];
+    void* handle = dlopen(path, RTLD_NOW);
     if(!handle) {
-        __android_log_print(ANDROID_LOG_ERROR, "dummyloader", "dl open '%s' failed (%s)",
-            path, strerror(errno));
+        sprintf(buffer, "%s%s", LIB_PATH, path);
+        handle = dlopen(buffer, RTLD_NOW);
+        if(!handle) {
+            __android_log_print(ANDROID_LOG_ERROR, "dummyloader", "dl open '%s' failed (%s)",
+                                path, strerror(errno));
             exit(1);
+        }
     }
 
     return handle;
@@ -21,9 +26,9 @@ void* load_lib(const char* path) {
 void ANativeActivity_onCreate(ANativeActivity * app, void * ud, size_t udsize)
 {
     __android_log_print(ANDROID_LOG_INFO, "dummyloader", "Loaded bootstrap");
-    load_lib(LIB_PATH "libcblite.so");
+    load_lib("libcblite.so");
     __android_log_print(ANDROID_LOG_INFO, "dummyloader", "Loaded CBL-C");
-    auto* main = (void (*)(ANativeActivity*, void*, size_t))dlsym(load_lib(LIB_PATH "libtestserver.so"), "ANativeActivity_onCreate");
+    auto* main = (void (*)(ANativeActivity*, void*, size_t))dlsym(load_lib( "libtestserver.so"), "ANativeActivity_onCreate");
     if (!main)  {
         __android_log_print(ANDROID_LOG_ERROR, "dummyloader", "undefined symbol ANativeActivity_onCreate");
         exit(1);
