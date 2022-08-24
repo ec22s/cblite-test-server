de0723549b8da63bbf6ac2adff5a6253519855f1
Abhay Aggrawal <abhayayay@Abhays-MacBook-Pro.local>
2022-08-24 16:10:47 +0530

    c scopes and collections

--- CBLClient/Apps/CBLTestServer-C/src/ScopeMethods.hpp
+++ CBLClient/Apps/CBLTestServer-C/src/ScopeMethods.hpp
@@ -6,6 +6,6 @@
 namespace scope_methods {
 
     void scope_scopeName(nlohmann::json& body, mg_connection* conn);
-    void scope_collectionName(nlohmann::json& body, mg_connection* conn);
+    void scope_collectionNames(nlohmann::json& body, mg_connection* conn);
     void scope_collection(nlohmann::json& body, mg_connection* conn);
 }
