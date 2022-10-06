8a4e499f609c8db98c70bea6698e869fbd4d6ccb
abhayayay <abhay.aggrawal@couchbase.com>
2022-10-06 10:35:33 +0530

    bulk docs in collectinos

--- CBLClient/Apps/CBLTestServer-iOS/CBLTestServer-iOS/Server/CollectionRequestHandler.swift
+++ CBLClient/Apps/CBLTestServer-iOS/CBLTestServer-iOS/Server/CollectionRequestHandler.swift
@@ -95,9 +95,54 @@ public class CollectionRequestHandler {
             let config = ValueIndexConfiguration(expression)
             try collection.createIndex(withName: name, config: config)          
             
+        case "collection_saveDocuments":
+            let database: Database = args.get(name:"database")!
+            let collection: Collection = args.get(name: "collection")!
+            let documents: Dictionary<String, Dictionary<String, Any>> = args.get(name: "documents")!
+            try database.inBatch {
+                for doc in documents {
+                    let id = doc.key
+                    var data: Dictionary<String, Any> = doc.value
+                    if data["_id"] != nil {
+                        data["id"] = id
+                        data.removeValue(forKey: "_id")
+                    }
+                    let new_data: Dictionary<String, Any> = setDataBlob(data)
+                    let document = MutableDocument(id: id, data: new_data)
+                    try! collection.save(document: document)
+                   }
+            }
+            
         default:
             throw RequestHandlerError.MethodNotFound(method)
         }
         return nil
+    }
+}
+private extension CollectionRequestHandler {
+    func setDataBlob(_ data: Dictionary<String, Any>) -> Dictionary<String, Any> {
+        guard let attachment_items = data["_attachments"] as? Dictionary<String, Dictionary<String, Any>> else {
+            return data
+        }
+        var existingBlobItems = [String: Any]()
+        var updatedData = data;
+        for (key, value) in attachment_items {
+            
+            if let d = value["data"] as? String  {
+            
+                let contentType = key.hasSuffix(".png") ? "image/jpeg" : "text/plain"
+                let blob = Blob(contentType: contentType, data: d.data(using: .utf8)!)
+                
+                updatedData[key] = blob
+                
+            } else if let _ = value["digest"] {
+                existingBlobItems[key] = value
+            }
+          }
+        updatedData.removeValue(forKey: "_attachments")
+        if !existingBlobItems.isEmpty {
+            updatedData["_attachments"] = existingBlobItems
+        }
+        return updatedData
     }
 }
