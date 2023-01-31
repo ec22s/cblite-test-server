07240d93b0dc2ffe3e491e1d36f61fd4c4122bca
Thuan Nguyen <soccon@gmail.com>
2023-01-31 11:03:42 -0800

    Update run.ps1
    Update package name

--- CBLClient/Apps/CBLTestServer-Dotnet/TestServer.UWP/run.ps1
+++ CBLClient/Apps/CBLTestServer-Dotnet/TestServer.UWP/run.ps1
@@ -2,8 +2,8 @@ param (
     [Parameter()][string]$AppDirectory
 )
 
-$PackageFullName="b0bd6c4c-e257-44c1-aeeb-7188f65af9d0_1.0.0.0_x64__75cr2b68sm664"
-$LaunchName="b0bd6c4c-e257-44c1-aeeb-7188f65af9d0_75cr2b68sm664!App"
+$PackageFullName="2d440d2f-7627-19ac-40fa-2dac74696c6c_1.0.0.0_x64__75cr2b68sm664"
+$LaunchName="2d440d2f-7627-19ac-40fa-2dac74696c6c_75cr2b68sm664!App"
 if(-Not $AppDirectory) {
     $AppDirectory = $PSScriptRoot
 }
