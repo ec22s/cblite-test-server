add58c93e89b182acec7230c0e144cbf699ac328
Jim Borden <jim.borden@couchbase.com>
2023-03-25 08:26:45 +0900

    Consolidate build script into one script to reduce redundancy

--- CBLClient/Apps/CBLTestServer-Dotnet/TestServer.Maui/build_all.ps1
+++ CBLClient/Apps/CBLTestServer-Dotnet/TestServer.Maui/build_all.ps1
@@ -8,6 +8,10 @@ function Modify-Packages {
     $ver = $args[1]
     $community = $args[2]
 
+    if(-Not $OnWindows) {
+        $filename = $filename.Replace("\", "/")
+    }
+
     $content = [System.IO.File]::ReadAllLines($filename)
     $checkNextLine = $false
     for($i = 0; $i -lt $content.Length; $i++) {
@@ -58,13 +62,81 @@ function Calculate-Version {
     return $version_to_use + "-b*"
 }
 
+function Build-WinUI {
+    Write-Host ""
+    Write-Host "  ************************"
+    Write-Host "  **** Building WinUI ****"
+    Write-Host "  ************************"
+    Write-Host ""
+
+    dotnet build -f net6.0-windows10.0.19041.0 -c Release TestServer.Maui.csproj
+    Push-Location ".\bin\Release\net6.0-windows10.0.19041.0\win10-x64\AppPackages\TestServer.Maui_1.0.0.1_Test\"
+    try {
+        7z a -r ${ZipPath}\TestServer.Maui.WinUI.zip *
+        7z a ${ZipPath}\TestServer.Maui.WinUI.zip $PSScriptRoot\run.ps1
+        7z a ${ZipPath}\TestServer.Maui.WinUI.zip $PSScriptRoot\stop.ps1
+    } finally {
+        Pop-Location
+    }
+}
+
+function Build-Android {
+    Write-Host ""
+    Write-Host "  **************************"
+    Write-Host "  **** Building Android ****"
+    Write-Host "  **************************"
+    Write-Host ""
+
+    dotnet publish -f net6.0-android -c Release TestServer.Maui.csproj
+    Push-Location ".\bin\Release\net6.0-android\publish"
+    Copy-Item *.apk $ZipPath\TestServer.Maui.Android.apk
+}
+
+function Build-Ios {
+    Write-Host ""
+    Write-Host "  **********************"
+    Write-Host "  **** Building iOS ****"
+    Write-Host "  **********************"
+    Write-Host ""
+
+    dotnet build -f net6.0-ios -c Release TestServer.Maui.csproj
+    dotnet build -f net6.0-ios -c Release -r ios-arm64 TestServer.Maui.csproj
+    Push-Location ".\bin\Release\net6.0-ios\iossimulator-x64"
+    zip -r $ZipPath/TestServer.Maui.iOS.zip TestServer.Maui.app
+    Pop-Location
+
+    Push-Location ".\bin\Release\net6.0-ios\ios-arm64"
+    zip -r $ZipPath/TestServer.Maui.iOS-Device.zip TestServer.Maui.app
+    Pop-Location
+}
+
+function Build-Mac {
+    Write-Host ""
+    Write-Host "  *************************"
+    Write-Host "  **** Building macOS ****"
+    Write-Host "  *************************"
+    Write-Host ""
+
+    dotnet build -f net6.0-maccatalyst -c Release TestServer.Maui.csproj
+    Push-Location ".\bin\Release\net6.0-maccatalyst\maccatalyst-x64"
+    zip -r $ZipPath/TestServer.Maui.macOS.zip TestServer.Maui.app
+    Pop-Location
+}
+
+$OnWindows = $PSVersionTable.PSVersion.Major -lt 6 -or $IsWindows
+if(-Not $OnWindows -and -Not $IsMacOS) {
+    throw "Script not supported on Linux"
+}
+
+
 
 Push-Location $PSScriptRoot
 $fullVersion = Calculate-Version
-$ZipPath = Resolve-Path ".\zips"
-if(-Not (Test-Path $ZipPath)) {
-    New-Item -ItemType Directory $ZipPath 
+if(-Not (Test-Path "zips")) {
+    New-Item -ItemType Directory "zips" 
 }
+
+$ZipPath = Resolve-Path "zips"
 
 try {
     Modify-Packages "$PSScriptRoot\..\TestServer\TestServer.csproj" $fullVersion $Community
@@ -74,31 +146,15 @@ try {
         exit 1
     }
 
-    Write-Host ""
-    Write-Host "  ************************"
-    Write-Host "  **** Building WinUI ****"
-    Write-Host "  ************************"
-    Write-Host ""
-
-    dotnet build -f net6.0-windows10.0.19041.0 -c Release .\TestServer.Maui.csproj
-    Push-Location ".\bin\Release\net6.0-windows10.0.19041.0\win10-x64\AppPackages\TestServer.Maui_1.0.0.1_Test\"
-    try {
-        7z a -r ${ZipPath}\TestServer.WinUI.zip *
-        7z a ${ZipPath}\TestServer.WinUI.zip $PSScriptRoot\run.ps1
-        7z a ${ZipPath}\TestServer.WinUI.zip $PSScriptRoot\stop.ps1
-    } finally {
-        Pop-Location
+    if($OnWindows) {
+        Build-WinUI
+        Build-Android
+    } else {
+        Build-Ios
+        Build-Mac
     }
 
-    Write-Host ""
-    Write-Host "  **************************"
-    Write-Host "  **** Building Android ****"
-    Write-Host "  **************************"
-    Write-Host ""
-
-    dotnet build -f net6.0-android -c Release .\TestServer.Maui.csproj
-    Push-Location ".\bin\Release\net6.0-android\publish"
-    Copy-Item *.apk $ZipPath\TestServer.Android.apk
+    
 } finally {
     Pop-Location
 }
